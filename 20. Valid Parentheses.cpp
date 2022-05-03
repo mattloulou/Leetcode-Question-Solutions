1b844bd03e3d8195b4b71711919048a45eb6946e
mattloulou <mattloulou123123@gmail.com>
2022-05-03 17:47:15 -0400

    Second day of grind75 challenge

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -1,7 +1,36 @@
-// using namespace std;
-// class Solution {
-// public:
-//     bool isValid(string s) {
-        
-//     }
-// };
+#include<string>
+#include<unordered_map>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> map{{'{','}'}, {'[',']'}, {'(',')'}};
+        stack<char> inputs;
+        for (const char& c : s) {
+
+            //if c is an open bracket
+            if (map.count(c)) {
+                inputs.push(c);
+
+            //if c is a closing bracket
+            } else {
+
+                // if the stack is empty, then we can't be closing.
+                if (inputs.size() == 0) {
+                    return false;
+                }
+
+                //if it properly closes a bracket
+                else if (map.at(inputs.top()) == c) {
+                    inputs.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        //check if all brackets were properly closed
+        return inputs.empty();
+    }
+};
