983a11faec8ff540b0585a7d09f394f1b467b570
mattloulou <mattloulou123123@gmail.com>
2022-05-04 17:19:30 -0400

    added comments to my solution

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -14,7 +14,11 @@ public:
         ListNode new_list{}; // default constructor call. Creates new stack object
         ListNode* head = &new_list;
         ListNode* current = &new_list;
+
+        //while there are still unsorted nodes:
         while (list1 != nullptr || list2 != nullptr) {
+
+            //case where both lists have nodes
             if (list1 != nullptr && list2 != nullptr) {
                 if (list1->val < list2->val) {
                     current->next = list1;
@@ -25,19 +29,24 @@ public:
                 }
             }
 
+            //case where only the first list has nodes remaining
             else if (list1 != nullptr) {
                 current->next = list1;
                 list1 = list1->next;
             }
 
+            //case where only the second list has nodes remaining
             else {
                 current->next = list2;
                 list2 = list2->next;
             }
 
+            //all cycles must move current forward one.
             current = current->next;
         }
     
+        //since the node head points to is just a fake node used to let us have a simple algorithm (always can do current->next),
+        //and is stack allocated (so auto deletion), we return the next node.
         return head->next;
     }
 };
