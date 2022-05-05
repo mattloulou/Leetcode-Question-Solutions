eb0adf6b29fa6b6d671fff14d96aba7703963731
mattloulou <mattloulou123123@gmail.com>
2022-05-05 17:31:04 -0400

    Completed day 4!

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices.at(0);
+        int max_profit = 0;
+
+        //loop through each element and keep updating the minimum price, and compare it to the current value.
+        for (int i = 1; i < prices.size(); ++i) {
+            
+            //check if we have a new min
+            if (prices.at(i) < min) {
+                min = prices.at(i);
+            }
+
+            const int current_profit = prices.at(i) - min;
+            max_profit = current_profit > max_profit ? current_profit: max_profit;
+        }
+        
+        return max_profit;
+    }
+
+
+};
