3b213268dec1b165de3a10b7ddc7c753d0fe6e4b
mattloulou <mattloulou123123@gmail.com>
2022-05-19 18:13:56 -0400

    Added comments explaining the code.

--- 278. First Bad Version.cpp
+++ 278. First Bad Version.cpp
@@ -6,15 +6,25 @@ public:
     int firstBadVersion(int n) {
         ul start = 1;
         ul end = n;
-        ul guess = (start + end)/2;
+        ul mid = (start + end)/2;
 
+        //we keep using binary search until start == end, which will be when both are on the very first bad version.
         while (start < end) {
-            if (!isBadVersion(guess)) { //this is a magical function given by leetcode
-                start = guess + 1;
+            if (!isBadVersion(mid)) { //this is a magical function given by leetcode
+
+                //if mid is not bad, then the first bad version will be after mid.
+                start = mid + 1;
+                
             } else {
-                end = guess;
+
+                //the reason there is no -1 is because if mid is the first bad version, we don't want end to go before it.
+                //In that case, the algorithm will still work. This is because as long as the range is > 1, mid will never be the same as end 
+                //(due to interger addition), which means start will keep getting larger until it reaches end.
+
+                //if mid is bad, then the first bad version will be <= mid, so end gets moved to exactly mid.
+                end = mid; 
             }
-            guess = (start + end)/2;
+            mid = (start + end)/2;
         }
         return start; //now they should be equal to each other
     }
