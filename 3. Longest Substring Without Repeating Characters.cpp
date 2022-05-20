847c898dc72167ba890ad13c90763aa6c1cb194f
mattloulou <mattloulou123123@gmail.com>
2022-05-20 17:38:40 -0400

    Completed another problem. This is based on the "sliding window" concept.

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> index_last_seen(128,-1); //needs all alphanumeric + symbols + spaces? So I will give it 0-127 in ASCII.
+        int current_streak = 0;
+        int max_streak = 0;
+
+        //loop through all chars
+        for (int i = 0; i < s.length(); ++i) {
+            
+            const int current_streak_starting_index = i-current_streak;
+
+            //if the current char is not within the current streak
+            if (index_last_seen[s.at(i)] < current_streak_starting_index) {
+                ++current_streak;
+                max_streak = max(current_streak, max_streak);
+
+            //the current char is within the current streak --> find the length of the new streak && update last seen index
+            } else {
+              current_streak = i-index_last_seen[s.at(i)];  
+            }
+
+            
+            index_last_seen[s.at(i)] = i;
+        }
+        return max_streak;
+
+    }
+};
