a32964c5d97d9fd8e77bea9335c22f239169070c
mattloulou <mattloulou123123@gmail.com>
2022-05-28 22:42:19 -0400

    Completed a medium question! I also started a hard question but do not feel like finishing it tonight.

---
+++ 0007. Reverse Integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        //capture if x is negative or not to reapply to the final result
+        bool negative = false;
+        if (x < 0) {
+            negative = true;
+        }
+
+        //store the flipped int in a long (to prevent overflow). This will work if x is negative or positive
+        long result = 0;
+        while (x != 0) {
+            int digit = abs(x % 10);
+            x/=10;
+            result *= 10; //MISTAKE: I originally put this statement after `result += digit;` instead of before like it is here
+            result += digit;
+            
+        }
+
+        if (negative) result *= -1;
+
+        //we will just cast it to an int if
+        return (result > INT_MAX || result < INT_MIN) ? 0: static_cast<int>(result); 
+    }
+};
