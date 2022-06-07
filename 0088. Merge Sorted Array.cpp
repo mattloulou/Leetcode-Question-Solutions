33a91dd036c91a1f124a0c9f0106913d0a5f8d8b
mattloulou <mattloulou123123@gmail.com>
2022-06-07 17:54:25 -0400

    Added a second solution

--- 0088. Merge Sorted Array.cpp
+++ 0088. Merge Sorted Array.cpp
@@ -24,3 +24,27 @@ public:
         nums1 = combined;
     }
 };
+
+// V2 sorting in nums1 from back to front (slightly better memory than V1 since we don't need a new array).
+// It is the same as my V1 but it was inspired by https://leetcode.com/problems/merge-sorted-array/discuss/29614/C%2B%2B-solution-runtime-O(n)-in-place-easy-to-understand
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int n1 = m-1, n2 = n-1, combined_index = m+n-1;
+
+        // while the index for both given arrays are within range, keep inserting the largest
+        while (n1 >= 0 && n2 >= 0) {
+            if (nums1[n1] > nums2[n2]) {
+
+                // assign the appropriate value and increment both pointers
+                nums1[combined_index--] = nums1[n1--];
+            } else {
+                nums1[combined_index--] = nums2[n2--];
+            }
+        }
+
+        // add in the rest of the numbers
+        while (n1 >= 0) nums1[combined_index--] = nums1[n1--];
+        while (n2 >= 0) nums1[combined_index--] = nums2[n2--];
+    }
+};
