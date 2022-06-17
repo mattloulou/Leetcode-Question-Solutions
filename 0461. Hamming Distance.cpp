136fdff32d693e52dd6a08194bf62d5f1c155252
mattloulou <mattloulou123123@gmail.com>
2022-06-17 08:48:04 -0400

    Completed an easy question

---
+++ 0461. Hamming Distance.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int answer = 0;
+        while (x > 0 || y > 0) {
+            answer += ((x&1) != (y&1)); // check if the first bits differ
+            x >>= 1;
+            y >>= 1;
+        }
+        return answer;
+    }
+};
+
+//V2 using an alternate method: https://leetcode.com/problems/hamming-distance/discuss/94705/My-C%2B%2B-solution-using-bit-manipulation
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int answer = 0, n = x ^ y; // n will have a set-bit wherever x and y differ.
+        while (n) {
+            ++answer;
+            n &= n-1; // pop the Least Significant Set-bit.
+        }
+        return answer;
+    }
+};
