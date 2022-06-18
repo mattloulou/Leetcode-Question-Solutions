1be8c0bec819c9ab80bc01f508ad06853d04c6fb
mattloulou <mattloulou123123@gmail.com>
2022-06-18 13:30:14 -0400

    Added another solution

--- 1323. Maximum 69 Number.cpp
+++ 1323. Maximum 69 Number.cpp
@@ -1,4 +1,6 @@
+#include <string>
 using namespace std;
+
 class Solution {
 public:
     int maximum69Number (int num) {
@@ -21,5 +23,22 @@ public:
         for (int i = 1; i <= highest_digit; ++i) addition*=10;
         addition = (highest_digit >= 0) ? addition: 0; // if highest_digit == -1, then there were no 6s (num is made from only 9s).
         return num + addition;
+    }
+};
+
+
+// V2; easier
+class Solution {
+public:
+    int maximum69Number (int num) {
+        // simply change the 6 in the most significant digit to a 9.
+        string number = to_string(num);
+        for (int i = 0; i < number.size(); ++i) {
+            if (number[i] == '6') {
+                number[i] = '9';
+                return stoi(number);
+            }
+        }
+        return num;
     }
 };
